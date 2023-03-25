d1749db0305bd80e08c9dee59af56f14c3d3d39d
Андрей Боев <a.boev.it@gmail.com>
2023-03-25 14:58:38 +0300

    Add project files.

---
+++ Orthographics/MainForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Orthographics
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            MainMenuControl mmc = new MainMenuControl();
+            mmc.Left = (this.ClientSize.Width - mmc.Width) / 2;
+            mmc.Top = (this.ClientSize.Height - mmc.Height) / 2;
+            MainPanel.Controls.Add(mmc);
+
+        }
+    }
+}
